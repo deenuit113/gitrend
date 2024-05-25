69403bc33e6458a65c501ab6f9c439054ac944a1
deenuit113 <leejh5314@gmail.com>
2024-05-25 22:58:57 +0900

    Fix: fix wordcloud error window 객체 정의

--- gitrend/components/WordCloud.tsx
+++ gitrend/components/WordCloud.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from 'react';
-import wordcloud from 'wordcloud';
 
 interface Word {
   text: string;
@@ -14,21 +13,28 @@ const WordCloud: React.FC<WordCloudProps> = ({ words }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const wordList = words.map(word => [word.text, word.value]);
+    const loadWordCloud = async () => {
+      if (typeof window !== 'undefined') {
+        const wordcloud = (await import('wordcloud')).default;
+        const canvas = canvasRef.current;
+        if (canvas) {
+          const wordList = words.map(word => [word.text, word.value]);
 
-      wordcloud(canvas, {
-        list: wordList,
-        gridSize: Math.round(16 * window.devicePixelRatio),
-        weightFactor: 2,
-        fontFamily: 'Times, serif',
-        color: 'random-dark',
-        rotateRatio: 0.5,
-        rotationSteps: 2,
-        backgroundColor: '#f0f0f0',
-      });
-    }
+          wordcloud(canvas, {
+            list: wordList,
+            gridSize: Math.round(16 * window.devicePixelRatio),
+            weightFactor: 2,
+            fontFamily: 'Times, serif',
+            color: 'random-dark',
+            rotateRatio: 0.5,
+            rotationSteps: 2,
+            backgroundColor: '#f0f0f0',
+          });
+        }
+      }
+    };
+
+    loadWordCloud();
   }, [words]);
 
   return <canvas ref={canvasRef} width={800} height={400} />;
