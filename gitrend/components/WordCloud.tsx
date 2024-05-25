72425597fa1dbff0e6987096b969c9c329ee6da0
deenuit113 <leejh5314@gmail.com>
2024-05-25 22:55:33 +0900

    Feat: add d3 WordCloud

---
+++ gitrend/components/WordCloud.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef } from 'react';
+import wordcloud from 'wordcloud';
+
+interface Word {
+  text: string;
+  value: number;
+}
+
+interface WordCloudProps {
+  words: Word[];
+}
+
+const WordCloud: React.FC<WordCloudProps> = ({ words }) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const wordList = words.map(word => [word.text, word.value]);
+
+      wordcloud(canvas, {
+        list: wordList,
+        gridSize: Math.round(16 * window.devicePixelRatio),
+        weightFactor: 2,
+        fontFamily: 'Times, serif',
+        color: 'random-dark',
+        rotateRatio: 0.5,
+        rotationSteps: 2,
+        backgroundColor: '#f0f0f0',
+      });
+    }
+  }, [words]);
+
+  return <canvas ref={canvasRef} width={800} height={400} />;
+};
+
+export default WordCloud;
