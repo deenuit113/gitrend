92a1119157bb9c03fa5c4b0c61e81fd60e261e2e
deenuit113 <leejh5314@gmail.com>
2024-05-28 17:02:41 +0900

    Feat: modify github scaper

--- gitrend/GithubRepoScraper.js
+++ gitrend/GithubRepoScraper.js
@@ -1,41 +1,44 @@
-// puppeteer을 가져온다.
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+import React, { useEffect, useState } from 'react';
+import puppeteer from 'puppeteer';
+import cheerio from 'cheerio';
 
-(async() => {
-  // 브라우저를 실행한다.
-  // 옵션으로 headless모드를 끌 수 있다.
-  const browser = await puppeteer.launch({
-    headless: false
-  });
+const RepoNameList: React.FC = () => {
+  const [repoNames, setRepoNames] = useState<string[]>([]);
 
-  // 새로운 페이지를 연다.
-  const page = await browser.newPage();
-  // 페이지의 크기를 설정한다.
-  await page.setViewport({
-    width: 1366,
-    height: 768
-  });
-  // "https://www.goodchoice.kr/product/search/2" URL에 접속한다. (여기어때 호텔 페이지)
-  await page.goto('https://github.com/trending');
+  useEffect(() => {
+    const fetchRepoNames = async () => {
+      const browser = await puppeteer.launch({ headless: false });
+      const page = await browser.newPage();
+      await page.setViewport({ width: 1366, height: 768 });
+      await page.goto('https://github.com/trending');
 
-  const content = await page.content();
-  // $에 cheerio를 로드한다.
-  const $ = cheerio.load(content);
-  // 복사한 리스트의 Selector로 리스트를 모두 가져온다.
-  const articles = $('body > div.logged-out.env-production.page-responsive > div.application-main > main > div.position-relative.container-lg.p-responsive.pt-6 > div > div:nth-child(2) article');
+      const content = await page.content();
+      const $ = cheerio.load(content);
+      const articles = $('body > div.logged-out.env-production.page-responsive > div.application-main > main > div.position-relative.container-lg.p-responsive.pt-6 > div > div:nth-child(2) article');
 
-    // 각 article 태그 안의 a 태그의 href 속성을 가져와서 리스트에 추가한다.
-    const RepoNameList = [];
-    articles.each((index, article) => {
+      const names: string[] = [];
+      articles.each((index, article) => {
         const articleHrefs = $(article).find('h2 a').map((i, a) => $(a).attr('href')).get();
-        RepoNameList.push(...articleHrefs);
-    });
+        names.push(...articleHrefs);
+      });
 
-    // href 속성들을 출력한다.
-    console.log(RepoNameList);
+      setRepoNames(names);
+      browser.close();
+    };
 
-    browser.close();
+    fetchRepoNames();
+  }, []);
 
-    module.exports = RepoNameList;
-})();
+  return (
+    <div>
+      <h2>Repositories</h2>
+      <ul>
+        {repoNames.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RepoNameList;
--- gitrend/GithubTopicScraper.js
+++ gitrend/GithubTopicScraper.js
@@ -1,42 +1,44 @@
-// puppeteer을 가져온다.
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+import React, { useEffect, useState } from 'react';
+import puppeteer from 'puppeteer';
+import cheerio from 'cheerio';
 
-(async() => {
-  // 브라우저를 실행한다.
-  // 옵션으로 headless모드를 끌 수 있다.
-  const browser = await puppeteer.launch({
-    headless: false
-  });
+const TopicNameList: React.FC = () => {
+  const [topics, setTopics] = useState<string[]>([]);
 
-  // 새로운 페이지를 연다.
-  const page = await browser.newPage();
-  // 페이지의 크기를 설정한다.
-  await page.setViewport({
-    width: 1366,
-    height: 768
-  });
-  // "https://www.goodchoice.kr/product/search/2" URL에 접속한다. (여기어때 호텔 페이지)
-  await page.goto('https://github.com/topics');
-  
-  const content = await page.content();
-  // $에 cheerio를 로드한다.
-  const $ = cheerio.load(content);
-  // 복사한 리스트의 Selector로 리스트를 모두 가져온다.
-  const TopicList = $("body > div.logged-out.env-production.page-responsive > div.application-main > main > div.d-lg-flex.container-lg.p-responsive > div.col-lg-3 > ul > li");
-  // 모든 리스트를 순환한다.
-  const TopicNameList = [];
-  TopicList.each((index, list) => {
-    const name = $(list).find("a").text().trim();
-    TopicNameList.push(`/topics/${name}`);
+  useEffect(() => {
+    const fetchTopics = async () => {
+      const browser = await puppeteer.launch({ headless: false });
+      const page = await browser.newPage();
+      await page.setViewport({ width: 1366, height: 768 });
+      await page.goto('https://github.com/topics');
 
-    //console.log({index, name});
-  });
+      const content = await page.content();
+      const $ = cheerio.load(content);
+      const topicList = $("body > div.logged-out.env-production.page-responsive > div.application-main > main > div.d-lg-flex.container-lg.p-responsive > div.col-lg-3 > ul > li");
 
-  console.log(TopicNameList);
+      const names: string[] = [];
+      topicList.each((index, list) => {
+        const name = $(list).find("a").text().trim();
+        names.push(`/topics/${name}`);
+      });
 
-  browser.close();
-  
-  module.exports = TopicNameList;
-})();
+      setTopics(names);
+      browser.close();
+    };
 
+    fetchTopics();
+  }, []);
+
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        {topics.map((topic, index) => (
+          <li key={index}>{topic}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TopicNameList;
