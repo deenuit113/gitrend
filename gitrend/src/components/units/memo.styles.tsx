c6abb2a0fd7393b745e8de71cfd29d40ae6d0392
deenuit113 <leejh5314@gmail.com>
2024-06-13 23:22:31 +0900

    Design: modify memo design

--- gitrend/src/components/units/memo.styles.tsx
+++ gitrend/src/components/units/memo.styles.tsx
@@ -21,10 +21,10 @@ const slideOut = keyframes`
 
 export const MemoContainer = styled.div<{ isVisible: boolean }>`
     position: fixed;
-    top: 50px;
+    top: 10%;
     left: 0;
     width: 300px;
-    height: 400px;
+    height: 77%;
     background-color: white;
     border: 1px solid black;
     padding: 10px;
@@ -33,11 +33,18 @@ export const MemoContainer = styled.div<{ isVisible: boolean }>`
 `;
 
 export const TextArea = styled.textarea`
+    font-family: Noto Sans Korean;
     width: 100%;
     height: calc(100% - 40px);
+    font-size: 25px;
+    font-weight: bold;
+    resize: none;
 `;
 
 export const DeleteButton = styled.button`
+    font-family: Noto Sans Korean;
+    font-size: 15px;
+    font-weight: bold;
     position: absolute;
     bottom: 10px;
     right: 10px;
@@ -45,10 +52,12 @@ export const DeleteButton = styled.button`
 
 export const ToggleButton = styled.button<{ isMemoVisible: boolean }>`
     position: fixed;
-    top: 50px;
+    top: 10%;
     left: ${({ isMemoVisible }) => (isMemoVisible ? '320px' : '0px')};
     z-index: 1001;
     transition: left 0.5s ease;
     writing-mode: vertical-rl;
     text-orientation: upright;
+    font-size: 15px;
+    font-weight: bolder;
 `;
