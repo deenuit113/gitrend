aa97262adbe54acd8b7223aed4140f32d5e36141
deenuit113 <leejh5314@gmail.com>
2024-06-13 21:59:14 +0900

    Feat: add memo delete function

--- gitrend/src/components/units/memo.tsx
+++ gitrend/src/components/units/memo.tsx
@@ -16,13 +16,24 @@ export default function Memo(): JSX.Element {
         localStorage.setItem('memoContent', newContent);
     };
 
+    const handleClear = () => {
+        setContent('');
+        localStorage.removeItem('memoContent');
+    };
+
     return (
         <div style={{ position: 'fixed', top: '50px', left: '0', width: '300px', height: '400px', backgroundColor: 'white', border: '1px solid black', padding: '10px', zIndex: 1000 }}>
             <textarea
                 value={content}
                 onChange={handleChange}
-                style={{ width: '100%', height: '100%' }}
+                style={{ width: '100%', height: 'calc(100% - 40px)' }}
             />
+            <button
+                onClick={handleClear}
+                style={{ position: 'absolute', bottom: '10px', right: '10px' }}
+            >
+                삭제
+            </button>
         </div>
     );
 }
