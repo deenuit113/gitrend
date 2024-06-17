05d6ce9aad88d14c6347f4b2fc2812bc64dc4c82
deenuit113 <leejh5314@gmail.com>
2024-06-17 16:35:08 +0900

    Docs: add html lang attribute

--- gitrend/pages/_app.tsx
+++ gitrend/pages/_app.tsx
@@ -1,5 +1,11 @@
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  
+  return (
+    <>
+      <html lang="ko"></html>
+      <Component {...pageProps} />
+    </>
+  );
 }
