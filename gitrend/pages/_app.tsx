1fc902c55562efcc5e8b52d056b582808ebccc43
deenuit113 <leejh5314@gmail.com>
2024-06-17 16:46:15 +0900

    Docs: add page title and icon

--- gitrend/pages/_app.tsx
+++ gitrend/pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  
-  return (
-    <>
-      <html lang="ko"></html>
-      <Component {...pageProps} />
-    </>
-  );
+    return (
+        <>
+            <html lang="ko"></html>
+            <Head>
+                <title>Gitrend</title>
+                <link rel="icon" href="/wcag.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
